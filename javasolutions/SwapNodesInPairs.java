772d4d31c9ab72d034b72654ff8f57519d8e2cd0
Polo <pololee1990@gmail.com>
2016-11-15 22:45:17 -0800

    pl - swap nodes in pairs

---
+++ javasolutions/SwapNodesInPairs.java
@@ -0,0 +1,28 @@
+/**
+ * What if the number of nodes in the linked list has only odd number of nodes?
+ * The last node should note be swapped.
+ */
+
+package javasolutions;
+
+public class SwapNodesInPairs {
+  public ListNode swapPairs(ListNode head) {
+    ListNode dummyHead = new ListNode(0);
+    dummyHead.next = head;
+
+    ListNode prev = dummyHead;
+    ListNode current = head;
+
+    while (current != null && current.next != null) {
+      ListNode x = current;
+      ListNode y = current.next;
+      prev.next = y;
+      x.next = y.next;
+      y.next = x;
+      prev = x;
+      current = x.next;
+    }
+
+    return dummyHead.next;
+  }
+}
