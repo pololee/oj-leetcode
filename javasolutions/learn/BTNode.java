eb602cfff8d30e94d7ff0e04f5288294e3193092
Polo <pololee1990@gmail.com>
2016-11-29 22:53:38 -0800

    pl - add dummyTree in BTNode

--- javasolutions/learn/BTNode.java
+++ javasolutions/learn/BTNode.java
@@ -19,6 +19,24 @@ public class BTNode<T> {
 		return data.toString();
 	}
 
+	/**
+	 * 							A
+	 * 				B	 					C
+	 * 		D				E  						F
+	 *   				G
+	 */
+
+	public static BTNode<String> dummyTree() {
+		BTNode<String> root = new BTNode<String>("A");
+		root.left = new BTNode<String>("B");
+		root.right = new BTNode<String>("C");
+		root.left.left = new BTNode<String>("D");
+		root.left.right = new BTNode<String>("E");
+		root.right.right = new BTNode<String>("F");
+		root.left.right.left = new BTNode<String>("G");
+		return root;
+	}
+
 	public static void main(String[] args) {
 		BTNode<Integer> a = new BTNode<Integer>(10);
 		System.out.println("a: " + a.toString());
