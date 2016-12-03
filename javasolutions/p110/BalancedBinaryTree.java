f05075573641b0169f2197951edf0f1a34bfb9e2
Polo <xiao.li@appfolio.com>
2016-12-03 15:19:29 -0800

    pl - add BalancedBinaryTree checker faster version

--- javasolutions/p110/BalancedBinaryTree.java
+++ javasolutions/p110/BalancedBinaryTree.java
@@ -40,9 +40,42 @@ public class BalancedBinaryTree {
     return Math.max(depth(root.left), depth(root.right)) + 1; 
   }
 
+  /**
+   * Use -1 as depth to indicate that the tree rooted at this input node is not
+   * a balanced binary tree. And if subtree is not a binary tree, then the tree
+   * rooted at the current node is not a balanced binary tree.
+   */
+  
+  public boolean isBalancedFaster(TreeNode root) {
+    return depthFaster(root) != -1;
+  }
+
+  private int depthFaster(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int leftDepth = depthFaster(root.left);
+    if (leftDepth == -1 ) {
+      return -1;
+    }
+
+    int rightDepth = depthFaster(root.right);
+    if (rightDepth == -1) {
+      return -1;
+    }
+
+    if (Math.abs(leftDepth - rightDepth) > 1) {
+      return -1;
+    } else {
+      return Math.max(leftDepth, rightDepth) + 1;
+    }
+  }
+
   public static void main(String[] args) {
     TreeNode root = TreeNode.dummyTree();
     BalancedBinaryTree bbt = new BalancedBinaryTree();
     System.out.println(bbt.isBalanced(root));
+    System.out.println(bbt.isBalancedFaster(root));
   }
 }
