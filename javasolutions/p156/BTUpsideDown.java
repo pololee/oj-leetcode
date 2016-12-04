8a7c09dcdb487fc96f99dc19bdb6f589fffe84f4
Polo <xiao.li@appfolio.com>
2016-12-04 13:28:00 -0800

    pl - add BTUpsideDown

---
+++ javasolutions/p156/BTUpsideDown.java
@@ -0,0 +1,46 @@
+/**
+ * This is a stupid question, because you need to understand what it is really asking for?
+ *
+ * Here is an example:
+ *
+ * Original Tree
+ *             1
+ *       2          3
+ *    4     5
+ * After conversion:
+ *            4
+ *       5           2
+ *               3      1
+ *
+ * So what actually happened? Bullshit
+ *
+ * Let's say current node is p, its parent is parent,
+ * p.left = parent.right(which is its sibling)
+ * p.right = parent
+ */
+
+
+
+package javasolutions.p156;
+
+import javasolutions.TreeNode;
+
+public class BTUpsideDown {
+  public TreeNode upsideDown(TreeNode root) {
+    TreeNode current = root, parent = null, parentRight = null;
+
+    while (current != null) {
+      TreeNode left = current.left;
+
+      current.left = parentRight;
+      parentRight = current.right;
+
+      current.right = parent;
+      parent = current;
+
+      current = left;
+    }
+
+    return parent;
+  }
+}
