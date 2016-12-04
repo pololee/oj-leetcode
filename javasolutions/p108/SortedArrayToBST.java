dd206b850bde42ae1f7db7d36b4bdea2ac7330f4
Polo <xiao.li@appfolio.com>
2016-12-03 16:14:29 -0800

    pl - better way to handle corner case

--- javasolutions/p108/SortedArrayToBST.java
+++ javasolutions/p108/SortedArrayToBST.java
@@ -4,25 +4,19 @@ import javasolutions.TreeNode;
 
 public class SortedArrayToBST {
   public TreeNode convert(int[] nums) {
-    if (nums.length == 0) {
-      return null;
-    }
-    
     return convertToBST(nums, 0, nums.length - 1);
   }
 
   private TreeNode convertToBST(int[] numbers, int low, int high) {
+    if (low > high) {
+      return null;
+    }
+
     int middle = (low + high) / 2;
 
     TreeNode root = new TreeNode(numbers[middle]);
-    
-    if (middle > low) {
-      root.left = convertToBST(numbers, low, middle - 1);
-    }
-
-    if (middle < high) {
-      root.right = convertToBST(numbers, middle + 1, high);
-    }
+    root.left = convertToBST(numbers, low, middle - 1);
+    root.right = convertToBST(numbers, middle + 1, high);
 
     return root;
   }
