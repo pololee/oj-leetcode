772f926a1a4e8e6665ea8fb9239ad111dd33b944
Polo <pololee1990@gmail.com>
2016-12-26 18:55:32 -0800

    pl - add in-order travel iteratively

--- javasolutions/learnBT/BTInOrderTravel.java
+++ javasolutions/learnBT/BTInOrderTravel.java
@@ -1,6 +1,7 @@
 package javasolutions.learnBT;
 
 import javasolutions.learnBT.TreeNode;
+import java.util.Stack;
 
 public class BTInOrderTravel {
   public void recursive(TreeNode root) {
@@ -20,10 +21,39 @@ public class BTInOrderTravel {
     return output;
   }
 
+  public void iterative(TreeNode root) {
+    if(root == null) return;
+
+    Stack<TreeNode> stack = new Stack<>();
+    StringBuilder builder = new StringBuilder();
+
+    TreeNode node = root;
+    while(node != null) {
+      stack.push(node);
+      node = node.left;
+    }
+
+    while(!stack.empty()) {
+      TreeNode current = stack.pop();
+      builder.append(String.valueOf(current.val)).append("#");
+
+      if(current.right != null) {
+        TreeNode tmp = current.right;
+        while(tmp != null) {
+          stack.push(tmp);
+          tmp = tmp.left;
+        }
+      }
+    }
+
+    System.out.println(builder.toString());
+  }
+
   public static void main(String[] args) {
     TreeNode root = TreeNode.dummyTree();
 
     BTInOrderTravel travel = new BTInOrderTravel();
     travel.recursive(root);
+    travel.iterative(root);
   }
 }
