7b59b554cce521c2d5e406a9e187b0c936462935
Polo <pololee1990@gmail.com>
2017-03-06 23:23:33 -0800

    pl - add comments for p231 power of two

--- javasolutions/p231/PowerOfTwo.java
+++ javasolutions/p231/PowerOfTwo.java
@@ -19,6 +19,7 @@ public class PowerOfTwo {
     return count == 1;
   }
 
+  // Power of 2 means only one bit of n is '1', so use the trick n&(n-1)==0 to judge whether that is the case
   public boolean isPowerOfTwoBest(int n) {
     return n > 0 && (n & (n - 1)) == 0;
   }
