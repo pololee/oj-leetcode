e3ccab0da4488ce49311b489aa65d76bfc08dfb2
Polo <pololee1990@gmail.com>
2018-07-12 00:01:27 -0700

    pl - add better solution for p647 palindrome substring count

--- facebook/p647/Solution.py
+++ facebook/p647/Solution.py
@@ -38,10 +38,35 @@ class Solution:
 
         return count
 
+class BetterSolution:
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        count = 0
+        size = len(s)
+        for i in range(2 * size - 1):
+            start = i // 2
+            end = start + i % 2
+
+            while start >= 0 and end < size:
+                if s[start] == s[end]:
+                    count += 1
+                else:
+                    break
+                start -= 1
+                end += 1
+        return count
+
 def main():
     sol = Solution()
     print(sol.countSubstrings('abc'))
     print(sol.countSubstrings('aaa'))
+    better_sol = BetterSolution()
+    print(better_sol.countSubstrings('abc'))
+    print(better_sol.countSubstrings('aaa'))
 
 if __name__ == '__main__':
     main()
