e7d3147d215c0e1fe077ddb6b140b1b0a9e2e203
Polo <xiao.li@appfolio.com>
2018-07-21 12:52:06 -0700

    pl - add p338 counting bits

--- p338/Solution.py
+++ p338/Solution.py
@@ -5,13 +5,20 @@ class Solution:
         :rtype: List[int]
         """
         DP = [0 for _ in range(num + 1)]
-        
+
         base = 1
         next_base = base * 2
         for i in range(1, num + 1):
             DP[i] = 1 + DP[i - base]
 
-            if base - 1 == i:
-                base *= 2
-                next_base = base * 2
-        
+            if i + 1 == next_base:
+                base = next_base
+                next_base *= 2
+        return DP
+
+def main():
+    sol = Solution()
+    print(sol.countBits(5))
+
+if __name__ == '__main__':
+    main()
