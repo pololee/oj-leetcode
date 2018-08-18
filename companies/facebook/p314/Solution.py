cb49339513bbad67f956178dc25e595a1e196cac
Polo <pololee1990@gmail.com>
2018-08-18 15:04:01 -0700

    pl - fix binary tree vertical order traversal

--- companies/facebook/p314/Solution.py
+++ companies/facebook/p314/Solution.py
@@ -19,16 +19,21 @@ class Solution:
         queue = deque()
         queue.append((0, root))
 
+        min_col = 0
+        max_col = 0
+
         while queue:
             col_index, node = queue.popleft()
             col_map[col_index].append(node.val)
 
             if node.left:
                 queue.append((col_index-1, node.left))
+                min_col = min(min_col, col_index - 1)
             if node.right:
                 queue.append((col_index+1, node.right))
+                max_col = min(max_col, col_index + 1)
         
-        for col_index in sorted(col_map):
+        for col_index in range(min_col, max_col + 1):
             answer.append(col_map[col_index])
         
         return answer
