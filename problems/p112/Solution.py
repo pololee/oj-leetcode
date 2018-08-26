b45c46f88b2ace8d6e0694a76d0311023f762e62
Polo <pololee1990@gmail.com>
2018-08-26 12:39:13 -0700

    pl - add problem 112

---
+++ problems/p112/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasPathSum(self, root, sum):
+        if not root:
+            return False
+
+        if not root.left and not root.right and sum - root.val == 0:
+            return True
+
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
