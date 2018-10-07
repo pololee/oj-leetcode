5a998c956131f7d725107dd249de8acba733484d
Polo <pololee1990@gmail.com>
2018-10-06 21:06:41 -0700

    pl - add another exclusive time of functions

--- companies/uber/636.exclusive-time-of-functions.python3.py
+++ companies/uber/636.exclusive-time-of-functions.python3.py
@@ -23,6 +23,30 @@ class Solution:
                 ans[stack.pop()] += ftime - lastTimeStamp
             lastTimeStamp = ftime
         return ans
+    
+    def anotherExclusiveTime(self, n, logs):
+        if not logs or n > 2 * len(logs):
+            return ans
+        
+        ans = [0 for _ in range(n)]
+        fId, _, fTime = self.parse(logs[0])
+        stack = [fId]
+        prevTime = fTime
+        
+        for log in logs[1:]:
+            fId, fType, fTime = self.parse(log)
+            
+            if fType == "start":
+                if stack:
+                    ans[stack[-1]] += (fTime - prevTime)
+                stack.append(fId)
+                prevTime = fTime
+            else:
+                ans[stack[-1]] += (fTime - prevTime + 1)
+                stack.pop()
+                prevTime = fTime + 1
+        
+        return ans
 
     def parse(self, log):
         parsed = log.split(":")
